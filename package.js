6eead7f336d802b3227b3058b0952b89e03e5e0e
Christoph Herbst <chris.p.herbst@gmail.com>
2014-10-29 11:57:20 +0100

    use versions from meteor 1.0

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'cherbst:bidiweb',
   summary: 'Bidiweb packaged for Meteor',
-  version: '1.0.0',
+  version: '1.0.1',
   git: 'https://github.com/cherbst/meteor-bidiweb.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0');
+  api.versionsFrom('METEOR@1.0');
   api.addFiles([
       'bidiweb/bidi_helpers.js',
       'bidiweb/bidiweb.js'], 'client');
